07d597d6e71debc2bd2da6c5a19f4196af1ee3d8
marcelacruz10 <marcelacruz8705@gmail.com>
2024-04-10 20:57:57 -0500

    práctica de los conceptos

---
+++ Scripts/practicaConceptos.js
@@ -0,0 +1,10 @@
+//verificar si este conectado con el html
+console.log("Hola! soy la actividad de hoy");
+
+//traer los elementos que necesitan de su html, los que tienen identificador unico
+//id->los elementos que me traiga deben estar en una variable->debeser constante
+
+//document me accede a mi html
+//getElementbyID me traen un elemento en particular por su identificador
+//dentro de los parentesis DEBO PONER EL MISMO ID que puse e mi html
+const edadinput= document.getElementById("edadinput");
