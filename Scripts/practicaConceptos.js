c99659a844889bd09abe25513dfa04d3e2e3c26e
marcelacruz10 <marcelacruz8705@gmail.com>
2024-04-15 20:35:37 -0500

    termino de actividad

--- Scripts/practicaConceptos.js
+++ Scripts/practicaConceptos.js
@@ -7,4 +7,26 @@ console.log("Hola! soy la actividad de hoy");
 //document me accede a mi html
 //getElementbyID me traen un elemento en particular por su identificador
 //dentro de los parentesis DEBO PONER EL MISMO ID que puse e mi html
+// mi elemento 2
 const edadinput= document.getElementById("edadinput");
+
+// mi elemento 2
+//const btn = document.getElementById ("botón"); console.log (btn)
+function verificarEdad (){
+    //yo quiero que cada que le de clik al botón le de un mensjae de "hola"
+    console.log ("Entró en mi función verificar edad");
+}
+const mensaje = document.getElementById ("mensajeEdad");
+
+let edad = edadinput.value; 
+console.log (edad);
+
+//utilizar condicionales
+if(edad>= 18){
+    console.log ("Eres mayo de edad");  
+    mensaje.textContent = "Eres mayor de edad";
+}else {
+        console.log ("Eres menor de edad");
+        mensaje.textContent =" Eres menor de edad";
+    }
+
